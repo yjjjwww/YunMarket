2a0d4b3510b291002b38ea7057a92d6833f43b5f
yjjjwww <yjjjwww@naver.com>
2023-05-13 03:33:49 +0900

    test : [feature/seller-signin] Customer 로그인 기능 테스트 코드 작성
    - 정상적으로 로그인시 토큰 발급 테스트
    - 로그인 실패시 에러 응답 테스트
    - Controller, Service 로그인 기능 테스트

--- src/test/java/com/yjjjwww/yunmarket/seller/service/SellerServiceImplTest.java
+++ src/test/java/com/yjjjwww/yunmarket/seller/service/SellerServiceImplTest.java
@@ -1,16 +1,20 @@
 package com.yjjjwww.yunmarket.seller.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+import com.yjjjwww.yunmarket.config.JwtTokenProvider;
 import com.yjjjwww.yunmarket.exception.CustomException;
 import com.yjjjwww.yunmarket.exception.ErrorCode;
 import com.yjjjwww.yunmarket.seller.entity.Seller;
+import com.yjjjwww.yunmarket.seller.model.SellerSignInServiceForm;
 import com.yjjjwww.yunmarket.seller.model.SellerSignUpServiceForm;
 import com.yjjjwww.yunmarket.seller.repository.SellerRepository;
 import java.util.Optional;
@@ -19,12 +23,16 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 
 @ExtendWith(MockitoExtension.class)
 class SellerServiceImplTest {
 
   @Mock
   private SellerRepository sellerRepository;
+
+  @Mock
+  private JwtTokenProvider provider;
 
   @InjectMocks
   private SellerServiceImpl sellerService;
@@ -97,5 +105,73 @@ class SellerServiceImplTest {
 
     //then
     assertEquals(ErrorCode.INVALID_PHONE, exception.getErrorCode());
+  }
+
+  @Test
+  void sellerSignInSuccess() {
+    //given
+    SellerSignInServiceForm form = SellerSignInServiceForm.builder()
+        .email("yjjjwww@naver.com")
+        .password("zero1111")
+        .build();
+
+    Seller seller = Seller.builder()
+        .id(1L)
+        .email("yjjjwww@naver.com")
+        .password(BCrypt.hashpw("zero1111", BCrypt.gensalt()))
+        .build();
+
+    given(sellerRepository.findByEmail(anyString())).willReturn(Optional.ofNullable(seller));
+    given(provider.createToken(anyString(), anyLong(), any())).willReturn("JWT Token");
+
+    //when
+    String token = sellerService.signIn(form);
+
+    //then
+    assertNotNull(token);
+  }
+
+  @Test
+  void sellerSignInFail_LOGIN_CHECK_FAIL_No_Email() {
+    //given
+    SellerSignInServiceForm form = SellerSignInServiceForm.builder()
+        .email("yjjjwww@naver.com")
+        .password("zero1111")
+        .build();
+
+    given(sellerRepository.findByEmail(anyString())).willReturn(
+        Optional.ofNullable(Seller.builder().build()));
+
+    //when
+    CustomException exception = assertThrows(CustomException.class,
+        () -> sellerService.signIn(form));
+
+    //then
+    assertEquals(ErrorCode.LOGIN_CHECK_FAIL, exception.getErrorCode());
+  }
+
+  @Test
+  void sellerSignInFail_LOGIN_CHECK_FAIL_Invalid_Password() {
+    //given
+    SellerSignInServiceForm form = SellerSignInServiceForm.builder()
+        .email("yjjjwww@naver.com")
+        .password("zero1111")
+        .build();
+
+    Seller seller = Seller.builder()
+        .id(1L)
+        .email("yjjjwww@naver.com")
+        .password("Wrong Password")
+        .build();
+
+    given(sellerRepository.findByEmail(anyString())).willReturn(
+        Optional.ofNullable(seller));
+
+    //when
+    CustomException exception = assertThrows(CustomException.class,
+        () -> sellerService.signIn(form));
+
+    //then
+    assertEquals(ErrorCode.LOGIN_CHECK_FAIL, exception.getErrorCode());
   }
 }
